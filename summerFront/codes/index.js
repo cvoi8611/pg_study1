bd450caa72e3399ee0abc91849120c62d115268a
covi_d <covi@covisserver.mshome.net>
2023-08-08 15:39:00 +0000

    6week

--- summerFront/codes/index.js
+++ summerFront/codes/index.js
@@ -4,36 +4,92 @@ const mysql = require('mysql');
 const dbconfig = require('./config/dbinfo.js');
 const connection = mysql.createConnection(dbconfig);
 
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+
 const path = require('path');
 const cors = require('cors');
 const app = express();
 const port = 3000;
 
-/*
-app.get('/', (req, res) => {
-	connection.query('SELECT * FROM TB1', (error, rows) => {
-		if (error) throw error;
-		res.send(rows);
-	});
+app.listen(port, () => {
+	console.log(`Example app listening on port ${port}`);
 });
-*/
 
-
-//DB를 80포트로  아래 코드 활성화
-app.get('/api',(req,res) => {
-	connection.query('SELECT * FROM TB1', (error, rows) => {
-	if (error) throw error;
-	res.send(rows);
-	});
-});
+// app.get('/api',(req,res) => {
+// 	connection.query('SELECT * FROM TB1', (error, rows) => {
+// 	if (error) throw error;
+// 	res.send(rows);
+// 	});
+// });
 
 app.get('/',(req,res) => {
 	res.sendFile(path.join(__dirname,'./','front','main.html'));
 });
 
-app.listen(port, () => {
-	console.log(`Example app listening on port ${port}`);
+
+
+///////////////
+//// 회원가입, 로그인, 로그아웃
+///////////////
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname,'./','front','login.html'));
 });
+
+app.post('/login', (req, res) => {
+    if (req.session.user ? req.session.user.id == 'test' : false) {
+        res.redirect('/');
+    }
+    else if(req.body.id == 'covi' && req.body.pw == '8611') {
+        req.session.user = {
+            id: req.body.id,
+        };
+
+        res.setHeader('Set-Cookie', ['user=' + req.body.id]);
+        res.redirect('/');
+    }
+    else {
+        res.redirect('/login');
+    }
+});
+
+
+app.get('/signin', (req, res) => {
+    res.sendFile(path.join(__dirname,'./','front','signin.html'));
+});
+
+app.post('/signin', (req, res) => {
+	//json 데이터 추출
+	const { name, age, address } = req.body;
+	
+	const sql = 'INSERT INTO DB1 (user_name, user_id, user_pwd) VALUES (${user_name}, ${user_id}, ${user_pwd})';
+	connection.query(sql);
+});
+
+app.get('/logout', (req, res) => {
+    req.session.destroy((err) => {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            res.clearCookie('user');
+            res.redirect('/');
+        }
+    });
+});
+
+///////////
+// 라우팅
+///////////
+
+app.get('/posts/:pid' ,(req, res) => {
+	const pageid = req.params.pid;
+	//page id로 쿼리 실행
+	//이후 , SSR 방식에 따라 작성?
+	res.render('posts', { title: req.params.pid , contents: 'this is SSR page' + req.params.pid });
+})
 
 
 //express.js에서 SQL 작성 위한 코드
