f581b1d370c99eeb598f06b35661b8647c48378f
Covi_D <covi@covisserver.mshome.net>
2023-08-09 07:19:19 +0000

    6week

--- summerFront/codes/index.js
+++ summerFront/codes/index.js
@@ -27,23 +27,13 @@ app.use(
     })
 );
 
-
 app.listen(port, () => {
 	console.log(`Example app listening on port ${port}`);
 });
 
-// app.get('/api',(req,res) => {
-// 	connection.query('SELECT * FROM TB1', (error, rows) => {
-// 	if (error) throw error;
-// 	res.send(rows);
-// 	});
-// });
-
 app.get('/',(req,res) => {
 	res.sendFile(path.join(__dirname,'./','front','main.html'));
 });
-
-
 
 ///////////////
 //// 회원가입, 로그인, 로그아웃
@@ -105,42 +95,3 @@ app.get('/posts/:pid' ,(req, res) => {
 	//이후 , SSR 방식에 따라 작성?
 	res.render('posts', { title: req.params.pid , contents: 'this is SSR page' + req.params.pid });
 })
-
-
-//express.js에서 SQL 작성 위한 코드
-/*
-app.get('/api', (req, res) => {
-	connection.query('SELECT * FROM Test1', (error, rows) => {
-		if (error) throw error;
-		res.send(rows);
-	});
-});
-
-app.post('/api', (req, res) => {
-	//json 데이터 추출
-	const { name, age, address } = req.body;
-	
-	const sql = 'INSERT INTO Test1 (name, age, address) VALUES '(${name}, ${age}, ${address})'';
-	connection.query(sql);
-
-	res.redirect('http://ubserver/');
-});
-
-app.put('/api', (req, res) => {
-	const { name, age, address } = req.body;
-
-	const sql = 'UPDATE Test1 SET age = '${age}', address = '${address}' WHERE name = '${name}'';
-	connection.query(sql);
-
-	res.redirect('http://ubserver/');
-});
-
-app.delete('/api', (req, res) => {
-	const { name } = req.body;
-
-	const sql = 'DELETE FROM Test1 WHERE name = '${name}'';
-	connection.query(sql);
-
-	res.redirect('http://ubserver/');
-});
-*/
